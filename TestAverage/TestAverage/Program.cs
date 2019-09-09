68edf57aee31f17662ee55362811e5671d980efd
Brandon Chan <brandonhofaichan@gmail.com>
2019-09-09 08:28:29 -0500

    made TestAverage code work

--- TestAverage/TestAverage/Program.cs
+++ TestAverage/TestAverage/Program.cs
@@ -6,7 +6,19 @@ namespace TestAverage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your first score.");
+            var answer1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your second score.");
+            var answer2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your third score.");
+            var answer3 = int.Parse(Console.ReadLine());
+
+            int testAverage;
+            testAverage = (answer1 + answer2 + answer3) / 3;
+
+            Console.WriteLine($"Your average is {testAverage}");
         }
     }
 }
